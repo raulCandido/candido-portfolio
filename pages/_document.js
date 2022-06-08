d21c388826f7996c04769eb53784c7e87d71f8dc
Raul Candido <raul.candrl@gmail.com>
2022-06-08 18:29:02 -0300

    Ajustando page inicial

--- pages/_document.js
+++ pages/_document.js
@@ -8,26 +8,18 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
-      image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+      title: 'portfolio'
     }
 
     return (
-      <Html lang="en">
+      <Html lang="pt-br">
         <Head>
-          <meta name="robots" content="follow, index" />
+          <meta name="robots" content="follow" />
           <meta name="description" content={meta.description} />
           <meta property="og:site_name" content={meta.title} />
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
           <meta property="og:image" content={meta.image} />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@yourname" />
-          <meta name="twitter:title" content={meta.title} />
-          <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
         </Head>
         <body>
           <Main />
